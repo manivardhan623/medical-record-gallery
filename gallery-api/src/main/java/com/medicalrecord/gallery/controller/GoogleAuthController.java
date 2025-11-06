03ca50a2e1e2d6413d25a9e6d5892dd9777858b9
CHELLAMLLA JASWANTH REDDY <2410030140@klh.edu.in>
2025-11-06 20:27:29 +0530

    Update CORS configuration to include frontend URL

--- gallery-api/src/main/java/com/medicalrecord/gallery/controller/GoogleAuthController.java
+++ gallery-api/src/main/java/com/medicalrecord/gallery/controller/GoogleAuthController.java
@@ -10,7 +10,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = {"http://localhost:3000", "http://10.231.20.144:3000"}, allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
+@CrossOrigin(origins = {"http://localhost:3000", "http://10.231.20.144:3000", "${frontend.url}"}, allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 public class GoogleAuthController {
 
     @Autowired
